f4563f8ebda239d875640907a0d99e7b93b4646e
mpede <277870@VIA.DK>
2022-10-17 09:42:47 +0200

    Initial commit

---
+++ src/main/java/com/example/aflevering2/Aflevering2Application.java
@@ -0,0 +1,13 @@
+package com.example.aflevering2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aflevering2Application {
+
+    public static void main (String[] args) {
+        SpringApplication.run(Aflevering2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aflevering2/Aflevering2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aflevering2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aflevering2ApplicationTests {
+
+    @Test
+    void contextLoads () {
+    }
+
+}
